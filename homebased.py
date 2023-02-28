33fdd60f3081b5f5b98e0fa726ce73eed37f876c
Lawrence Logoh <lawrence.logoh@gmail.com>
2023-02-28 03:29:18 +0000

    Tried using handlers.py
    It's a bit messy but this is a prototype so it doesn't matter too
    much. Also added __pycache__ to .gitignore

--- homebased.py
+++ homebased.py
@@ -2,6 +2,7 @@ import tomli as tomllib
 from pathlib import Path
 from shutil import copy
 from xdg import xdg_config_home
+import handlers
 
 config_dir = xdg_config_home() / "homebase"
 config_file = config_dir / "config.toml"
@@ -17,7 +18,14 @@ def main():
 
     with open(config_file, "rb") as f:
         data = tomllib.load(f)
-        print(data)
+        links = data["Tags"]["blogs"][0]
+        entries = handlers.handle_rss(links)["entries"]
+        title = entries[0]["title"]
+        summary = entries[0]["summary"]
+        link = entries[0]["link"]
+        print(f"Title: {title}")
+        print(f"Title: {link}")
+        print(f"Title: {summary}")
 
 
 if __name__ == "__main__":
